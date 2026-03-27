8cb73222044b0549d76f5336a584f46acfec2035
Srijan Sarkar <srijanarkar.coding@gmail.com>
2026-03-27 20:17:43 +0530

    Changed the port code

--- server.js
+++ server.js
@@ -416,7 +416,7 @@ app.use((err, req, res, next) => {
 })
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running in ${PORT}`)
